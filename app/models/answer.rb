111ae11ed5511beb6a79e30cddbae7b68f3524a3
Mariko <maripiyok@gmail.com>
2015-10-20 08:25:26 +0900

    heroku err

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,5 +6,5 @@ class Answer < ActiveRecord::Base
 
   validates_presence_of :topic_id, :user_id, :answer_type
 
-  enumerize :answer_type, in: { '未回答': 0, '出席': 1, '欠席': -1 }
+  enumerize :answer_type, in: { 'unknown': 0, 'yes': 1, 'no': -1 }
 end
